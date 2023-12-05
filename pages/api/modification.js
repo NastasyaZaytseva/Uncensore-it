b3e4df2da34198448f2f4d3019d1ce9d166f7e42
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2023-12-05 16:26:25 +0100

    Change words modification function.

--- pages/api/modification.js
+++ pages/api/modification.js
@@ -1,27 +1,31 @@
-// pages/api/modification.js
-import modificationsData from '../api/modifications.json'; 
+import wordsToReplace from '../api/words-to-replace.json';
+import symbols from '../api/symbols-for-replacement.json';
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { text } = req.body;
-    const modifiedText = randomizeText(text);
+    const modifiedText = modifyText(text);
     res.status(200).json({ modifiedText });
   } else {
-    res.status(405).end(); 
+    res.status(405).end();
   }
 }
 
-function randomizeText(text) {
-  return text
-    .split(/\b/)
-    .map(word => {
-      const lowerWord = word.toLowerCase();
-      return modificationsData[lowerWord] ? getRandomElement(modificationsData[lowerWord]) : word;
-    })
-    .join('');
-}
+function modifyText(inputText) {
+  const wordsArr = wordsToReplace["Words"];
 
-function getRandomElement(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+  const inputWords = inputText.split(/\s+/);
+
+  const modifiedWords = inputWords.map(word => {
+    if (wordsArr.includes(word.toLowerCase())) {
+      const modifiedWord = word.split('').map(char => symbols[char.toLowerCase()].getRandomElement() || char).join('');
+      return modifiedWord.slice(0, -2) + word.slice(-2); // Replace (length - 2) characters
+    } else {
+      return word;
+    }
+  });
+
+  const modifiedText = modifiedWords.join(' ');
+
+  return modifiedText;
 }
