1730f60c620cec2093c881cc86241c9cabbbe7b0
Nastasya <nastasya@skiff.com>
2023-12-06 12:15:33 +0100

    Modify React component structure to accommodate layout changes

---
+++ components/ModifyText.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import useModification from './useModification';
+import InputArea from './InputArea';
+import ResultDisplay from './ResultDisplay';
+
+export default function ModifyText() {
+  const { inputText, setInputText, modifiedText, modifyText } = useModification();
+
+  return (
+    <div className="modify-text-container">
+      <h1>UNCENSOR IT</h1>
+      <div className="input-container">
+        <textarea
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="START TYPING HERE..."
+          className="textarea"
+        />
+        {modifiedText && (
+          <div className="modified-text-result">
+            <span>{modifiedText}</span>
+          </div>
+        )}
+      </div>
+      <button 
+        onClick={modifyText} 
+        className="button"
+      >
+        PRESS TO UNCENSOR IT
+      </button>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,7 @@
-import { useState } from 'react';
+import React from 'react';
+import ModifyText from '../components/ModifyText'; 
 
-export default function ModifyText() {
-  const [inputText, setInputText] = useState('');
-  const [modifiedText, setModifiedText] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch('/api/modification', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ text: inputText }),
-    });
-    const data = await response.json();
-    setModifiedText(data.modifiedText);
-  };
-
-  return (
-    <div className="modify-text-container">
-      <h1>Uncensor it</h1>
-      <div className="input-result-container">
-        <textarea
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder="START TYPING HERE..."
-          className="textarea"
-        />
-        {modifiedText && (
-          <div className="modified-text-result">
-            <span>{modifiedText}</span>
-          </div>
-        )}
-      </div>
-      <button 
-        onClick={handleSubmit} 
-        className="button"
-      >
-        PRESS TO UNCENSOR IT
-      </button>
-    </div>
-  );
-  
-  
+export default function Home() {
+  return <ModifyText />;
 }
