932909316deebd89f2c76d3dbf85a4bac61a7f83
Nas <nas@nass-mbp.speedport.ip>
2025-11-20 14:36:44 +0100

    visuals

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,24 @@ import '../styles/ModifyText.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  
+  // Determine background based on route
+  const getBackgroundClass = () => {
+    if (router.pathname === '/contribute') {
+      return 'bg-contribute';
+    }
+    if (router.pathname === '/about-us') {
+      return 'bg-about';
+    }
+    return 'bg-main';
+  };
+
   return (
-    <div className="container">
+    <div className={`container ${getBackgroundClass()}`}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
